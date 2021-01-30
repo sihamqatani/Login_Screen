34939b0ed08892102e6018bd2de86715ad33fd86
Nada <Nada@DESKTOP-6FOVJ24>
2021-01-30 23:54:42 +0300

    first

---
+++ lib/drawing_wedgets.dart
@@ -0,0 +1,137 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+Widget drawLogo() {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      Padding(
+        padding: const EdgeInsets.only(bottom: 50),
+        child: Text(
+          'LOGIN SCREEN',
+          style: TextStyle(
+              color: Colors.white, fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+      ),
+    ],
+  );
+}
+
+Widget drawSingIn(context) {
+  return Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+    ClipRRect(borderRadius: BorderRadius.all( Radius.circular(20),),
+    child:Container(
+        width: MediaQuery.of(context).size.width * 0.8,
+        height: MediaQuery.of(context).size.height * 0.7,
+        decoration: BoxDecoration(
+            color: Colors.white,),
+           // borderRadius: BorderRadius.only(
+               //topLeft: Radius.circular(30), topRight: Radius.circular(30))),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child:
+                  Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                Text(
+                  "Login",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 24,
+                      color: Colors.black54),
+                ),
+              ]),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                    labelText: 'Enter email',
+                    hintText: 'please enter your email',
+                    prefixIcon: Icon(
+                      Icons.email,
+                      color: Colors.lightBlueAccent,
+                    )),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                keyboardType: TextInputType.visiblePassword,
+                decoration: InputDecoration(
+                  prefixIcon: Icon(
+                    Icons.lock,
+                    color: Colors.lightBlueAccent,
+                  ),
+                  labelText: 'Enter Password',
+                  hintText: 'please enter passsword',
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: Row(
+                children: [
+                  FlatButton(
+                    child: Text(
+                      'forget password',
+                      style: TextStyle(color: Colors.black54),
+                    ),
+                    onPressed: () {},
+                  ),
+
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+
+                    child: RaisedButton(
+                        color: Colors.lightBlueAccent,
+                        child: Text(
+                          'Login',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold,
+                              letterSpacing:1 ),
+                        ),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20)),
+                        onPressed: () {}),height: 1.4 * (MediaQuery.of(context).size.height / 20),
+                    width: 5 * (MediaQuery.of(context).size.width / 10),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('-OR-',style: TextStyle(fontSize: 16),),
+              ],),
+            ),
+            SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(margin: EdgeInsets.only(bottom: 16),
+                      child: CircleAvatar(backgroundColor:Colors.lightBlueAccent ,child: Icon(FontAwesomeIcons.twitter,color: Colors.white,),))
+                ],),
+              ),
+            ),
+          ],
+        )),
+    )]);
+}
---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'drawing_wedgets.dart';
+
+void main() {
+  runApp(loginScreen());
+}
+
+class loginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: Stack(
+          children: [
+            Container(
+              height: MediaQuery.of(context).size.height * .8,
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                  color: Colors.lightBlueAccent,
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(50),
+                    bottomRight:Radius.circular(50),
+
+                  )),
+            ),
+            Column(mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+
+              drawLogo(),
+             drawSingIn(context),
+           //  drawButtom();
+            ],)
+
+          ],
+        ),
+      ),
+    );
+  }
+}
